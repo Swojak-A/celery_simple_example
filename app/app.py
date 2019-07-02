2a6d4786cce3b8dcfbf6fc05289ae4d0caf4ac96
aswojak <a.swojak@futuremind.com>
2019-07-02 13:02:18 +0200

    feat: add route for index and template for index.html

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for
 from celery import Celery
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 #Flask mail configuration  
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'  
@@ -15,23 +21,31 @@ app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False  
 app.config['MAIL_USE_SSL'] = True
 
-
 #instantiate the Mail class  
 mail = Mail(app)  
   
-#configure the Message class object and send the mail from a URL  
-@app.route('/')  
-def index():  
-    recipient = "pilot.string@gmail.com"
-    msg = Message('subject', sender = 'pilot.string@gmail.com', recipients=[recipient])  
-    msg.body = 'hi, this is the mail sent by using the flask web application'  
 
 
+# ROUTES
+@app.route('/', methods=['GET', 'POST'])  
+def index():
+	if request.method == 'GET':
+		return render_template('index.html')
 
-    mail.send(msg)
+	email = request.form['email']
 
-    return "Mail Sent"  
+	return redirect(url_for('index'))
+
+    # recipient = "pilot.string@gmail.com"
+    # msg = Message('subject', sender = 'pilot.string@gmail.com', recipients=[recipient])  
+    # msg.body = 'hi, this is the mail sent by using the flask web application'  
+
+    # mail.send(msg)
+
+    # return "Mail Sent"  
   
+
+
 if __name__ == '__main__':
 	app.run()
 
